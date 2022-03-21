fb71b8e029699697558b98fcb849e8fe7f40d878
Tomas Kašelynas <tomaskas98@gmail.com>
2022-03-21 11:15:45 +0200

    Create tests for Caesar cipher

---
+++ Ciphers/Ciphers.Tests/Ciphers/CaesarCipherTests.cs
@@ -0,0 +1,66 @@
+﻿using Ciphers.Caesar;
+using Ciphers.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciphers.Tests.Ciphers
+{
+    [TestClass]
+    public class CaesarCipherTests
+    {
+        [DataRow(3, "ABC", "DEF")]
+        [DataRow(3, "abc", "def")]
+        [DataRow(3, "XYZ", "ABC")]
+        [DataRow(3, "xyz", "abc")]
+        [DataRow(-3, "DEF", "ABC")]
+        [DataRow(-3, "def", "abc")]
+        [DataRow(-3, "ABC", "XYZ")]
+        [DataRow(-3, "abc", "xyz")]
+        [DataRow(29, "ABC", "DEF")]
+        [DataRow(29, "abc", "def")]
+        [DataRow(29, "XYZ", "ABC")]
+        [DataRow(29, "xyz", "abc")]
+        [DataRow(-29, "DEF", "ABC")]
+        [DataRow(-29, "def", "abc")]
+        [DataRow(-29, "ABC", "XYZ")]
+        [DataRow(-29, "abc", "xyz")]
+        [DataRow(3, "a123c", "d123f")]
+        [DataRow(-3, "D321F", "A321C")]
+        [DataTestMethod]
+        public void Encrypt_ReturnEncryptedMessage_WhenPlainTextIsGiven(int key, string plainText, string expectedEncryption)
+        {
+            CaesarCipher cipher = new CaesarCipher(key);
+
+            string actual = cipher.Encrypt(plainText);
+
+            Assert.AreEqual(expectedEncryption, actual);
+        }
+
+        [DataRow(3, "DEF", "ABC")]
+        [DataRow(3, "def", "abc")]
+        [DataRow(3, "ABC", "XYZ")]
+        [DataRow(3, "abc", "xyz")]
+        [DataRow(-3, "ABC", "DEF")]
+        [DataRow(-3, "abc", "def")]
+        [DataRow(-3, "XYZ", "ABC")]
+        [DataRow(-3, "xyz", "abc")]
+        [DataRow(29, "DEF", "ABC")]
+        [DataRow(29, "def", "abc")]
+        [DataRow(29, "ABC", "XYZ")]
+        [DataRow(29, "abc", "xyz")]
+        [DataRow(-29, "ABC", "DEF")]
+        [DataRow(-29, "abc", "def")]
+        [DataRow(-29, "XYZ", "ABC")]
+        [DataRow(-29, "xyz", "abc")]
+        [DataRow(3, "D321F", "A321C")]
+        [DataRow(-3, "a123c", "d123f")]
+        [DataTestMethod]
+        public void Decrypt_ReturnDecryptedMessage_WhenCipherTextIsGiven(int key, string cipherText, string expectedDecryption)
+        {
+            CaesarCipher cipher = new CaesarCipher(key);
+
+            string actual = cipher.Decrypt(cipherText);
+
+            Assert.AreEqual(expectedDecryption, actual);
+        }
+    }
+}
