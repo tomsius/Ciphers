45afaa532a4c3e9e9e6ce66ad6c40a596559d13c
Tomas Kašelynas <tomaskas98@gmail.com>
2022-03-21 11:14:32 +0200

    Create ascii manipulator utility

---
+++ Ciphers/Ciphers/Utility/AsciiManipulator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ciphers.Utility
+{
+    public static class AsciiManipulator
+    {
+        private const int UpperLetterAsciiStart = 'A';
+        private const int LowerLetterAsciiStart = 'a';
+
+        public static int NormalizeLetter(char letter)
+        {
+            if (!char.IsLetter(letter))
+            {
+                throw new ArgumentException("Argument has to be a letter.");
+            }
+
+            if (char.IsUpper(letter))
+            {
+                return NormalizeUpperLetter(letter);
+            }
+
+            return NormalizeLowerLetter(letter);
+        }
+
+        private static int NormalizeUpperLetter(char letter)
+        {
+            return letter - UpperLetterAsciiStart;
+        }
+
+        private static int NormalizeLowerLetter(char letter)
+        {
+            return letter - LowerLetterAsciiStart;
+        }
+
+        public static char ReverseToLetter(int letterIndex, bool isUpper)
+        {
+            if (isUpper)
+            {
+                return ReverseToUpperLetter(letterIndex);
+            }
+
+            return ReverseToLowerLetter(letterIndex);
+        }
+
+        private static char ReverseToUpperLetter(int letterIndex)
+        {
+            return (char)(letterIndex + UpperLetterAsciiStart);
+        }
+
+        private static char ReverseToLowerLetter(int letterIndex)
+        {
+            return (char)(letterIndex + LowerLetterAsciiStart);
+        }
+    }
+}
