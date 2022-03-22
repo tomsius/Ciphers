6f63892c0216e5919f2b04f8d462589eb25b567b
Tomas Kašelynas <tomaskas98@gmail.com>
2022-03-22 11:08:54 +0200

    Create tests for Vigenere cipher

---
+++ Ciphers/Ciphers.Tests/Ciphers/VigenereCipherTests.cs
@@ -0,0 +1,41 @@
+﻿using Ciphers.Caesar;
+using Ciphers.Ciphers;
+using Ciphers.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciphers.Tests.Ciphers
+{
+    [TestClass]
+    public class VigenereCipherTests
+    {
+        [DataRow("CAKE", "ABCABC", "CBMEDC")]
+        [DataRow("CAKE", "abcabc", "cbmedc")]
+        [DataRow("CAKE", "XYZXYZ", "ZYJBAZ")]
+        [DataRow("CAKE", "xyzxyz", "zyjbaz")]
+        [DataRow("CAKE", "a1234c", "c1234c")]
+        [DataTestMethod]
+        public void Encrypt_ReturnEncryptedMessage_WhenPlainTextIsGiven(string key, string plainText, string expectedEncryption)
+        {
+            VigenereCipher cipher = new VigenereCipher(key);
+
+            string actual = cipher.Encrypt(plainText);
+
+            Assert.AreEqual(expectedEncryption, actual);
+        }
+
+        [DataRow("CAKE", "CBMEDC", "ABCABC")]
+        [DataRow("CAKE", "cbmedc", "abcabc")]
+        [DataRow("CAKE", "ZYJBAZ", "XYZXYZ")]
+        [DataRow("CAKE", "zyjbaz", "xyzxyz")]
+        [DataRow("CAKE", "C1234C", "A1234C")]
+        [DataTestMethod]
+        public void Decrypt_ReturnDecryptedMessage_WhenCipherTextIsGiven(string key, string cipherText, string expectedDecryption)
+        {
+            VigenereCipher cipher = new VigenereCipher(key);
+
+            string actual = cipher.Decrypt(cipherText);
+
+            Assert.AreEqual(expectedDecryption, actual);
+        }
+    }
+}
