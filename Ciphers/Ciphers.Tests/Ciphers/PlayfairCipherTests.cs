fc0205778634a42bcd2026d46fbb32076a39d3a2
Tomas Kašelynas <tomaskas98@gmail.com>
2022-04-13 08:53:55 +0300

    Create tests for playfair cipher

---
+++ Ciphers/Ciphers.Tests/Ciphers/PlayfairCipherTests.cs
@@ -0,0 +1,42 @@
+﻿using Ciphers.Ciphers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciphers.Tests.Ciphers
+{
+    [TestClass]
+    public class PlayfairCipherTests
+    {
+        [DataRow("MONARCHY", "INSTRUMENTS", "GATLMZCLRQTX")]
+        [DataRow("monarchy", "instruments", "GATLMZCLRQTX")]
+        [DataRow("MONarCHY", "INSTrumENTS", "GATLMZCLRQTX")]
+        [DataRow("MONarCHY", "hello", "CFSUPM")]
+        [DataRow("MONarCHY", "helloe", "CFSUPMKU")]
+        [DataRow("japonija", "jonas", "ANIPUX")]
+        [DataTestMethod]
+        public void Encrypt_ReturnEncryptedMessage_WhenPlainTextIsGiven(string key, string plainText, string expectedEncryption)
+        {
+            PlayfairCipher cipher = new PlayfairCipher(key);
+
+            string actual = cipher.Encrypt(plainText);
+
+            Assert.AreEqual(expectedEncryption, actual);
+        }
+
+        [DataRow("MONARCHY", "GATLMZCLRQTX", "INSTRUMENTSZ")]
+        [DataRow("monarchy", "gatlmzclrqtx", "INSTRUMENTSZ")]
+        [DataRow("MONarCHY", "GATLmzcLRQTX", "INSTRUMENTSZ")]
+        [DataRow("MONarCHY", "CFSUPM", "HELXLO")]
+        [DataRow("MONarCHY", "CFSUPMKU", "HELXLOEZ")]
+        [DataRow("japonija", "ANIPUX", "IONASZ")]
+        [DataRow("MONARCHY", "MC", "UM")]
+        [DataTestMethod]
+        public void Decrypt_ReturnDecryptedMessage_WhenCipherTextIsGiven(string key, string cipherText, string expectedDecryption)
+        {
+            PlayfairCipher cipher = new PlayfairCipher(key);
+
+            string actual = cipher.Decrypt(cipherText);
+
+            Assert.AreEqual(expectedDecryption, actual);
+        }
+    }
+}
---
+++ Ciphers/Ciphers.Tests/Utility/PlayfairHelperTests.cs
@@ -0,0 +1,67 @@
+﻿using Ciphers.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Ciphers.Tests.Utility
+{
+    [TestClass]
+    public class PlayfairHelperTests
+    {
+        private readonly char[,] _keyMatrix;
+        public PlayfairHelperTests()
+        {
+            _keyMatrix = new char[2, 2];
+            _keyMatrix[0, 0] = 'A';
+            _keyMatrix[0, 1] = 'B';
+            _keyMatrix[1, 0] = 'C';
+            _keyMatrix[1, 1] = 'D';
+        }
+
+        [DataRow('%', "Argument has to be a letter.")]
+        [DataRow('1', "Argument has to be a letter.")]
+        [DataTestMethod]
+        public void GetAvailableLetter_ThrowArgumentException_WhenArgumentIsNotLetter(char givenInput, string expectedMessage)
+        {
+            var ex = Assert.ThrowsException<ArgumentException>(() => PlayfairHelper.GetAvailableLetter(givenInput));
+
+            Assert.AreEqual(expectedMessage, ex.Message);
+        }
+
+        [DataRow('A', 'A')]
+        [DataRow('a', 'a')]
+        [DataRow('I', 'I')]
+        [DataRow('J', 'I')]
+        [DataTestMethod]
+        public void GetAvailableLetter_ReturnAvailableLetter_WhenInputIsLetter(char givenLetter, char expectedLetter)
+        {
+            char actual = PlayfairHelper.GetAvailableLetter(givenLetter);
+
+            Assert.AreEqual(expectedLetter, actual);
+        }
+
+        [DataRow("AbC", "AbC")]
+        [DataRow("a123b", "ab")]
+        [DataRow("a b=1c", "abc")]
+        [DataTestMethod]
+        public void NormalizeText_ReturnTextWithLettersOnly_WhenInputIsText(string givenText, string expectedNormalizedText)
+        {
+            string actual = PlayfairHelper.NormalizeText(givenText);
+
+            Assert.AreEqual(expectedNormalizedText, actual);
+        }
+
+        [DataRow('A', 0, 0)]
+        [DataRow('B', 0, 1)]
+        [DataRow('C', 1, 0)]
+        [DataRow('D', 1, 1)]
+        [DataRow('E', -1, -1)]
+        [DataTestMethod]
+        public void GetPositionOfLetter_ReturnTupleForPositionOfLetterInMatrix_When(char letterToSearch, int expectedRow, int expectedCol)
+        {
+            (int actualRow, int actualCol) = PlayfairHelper.GetPositionOfLetter(_keyMatrix, letterToSearch);
+
+            Assert.AreEqual(expectedRow, actualRow);
+            Assert.AreEqual(expectedCol, actualCol);
+        }
+    }
+}
