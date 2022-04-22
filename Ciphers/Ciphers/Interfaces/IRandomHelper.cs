12f268ea8142fc7cd2ba8efe8f1c6b2601ca7c1f
Tomas Kašelynas <tomaskas98@gmail.com>
2022-04-22 13:49:18 +0300

    Wrap Random class for testing

---
+++ Ciphers/Ciphers/Interfaces/IRandomHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace Ciphers.Interfaces
+{
+    public interface IRandomHelper
+    {
+        int Next(int v);
+    }
+}
---
+++ Ciphers/Ciphers/Utility/RandomNumberGeneratorHelper.cs
@@ -0,0 +1,20 @@
+﻿using Ciphers.Interfaces;
+using System;
+
+namespace Ciphers.Utility
+{
+    public class RandomNumberGeneratorHelper : IRandomHelper
+    {
+        private readonly Random _random;
+
+        public RandomNumberGeneratorHelper()
+        {
+            _random = new Random();
+        }
+
+        public int Next(int max)
+{
+            return _random.Next('z' - 'a' + 1);
+        }
+    }
+}
