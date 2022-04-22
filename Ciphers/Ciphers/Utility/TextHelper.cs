03e75924f1cf484846adf158461e21f8ef64625f
Tomas Kašelynas <tomaskas98@gmail.com>
2022-04-22 13:49:45 +0300

    Add text helper to leave only letters in text

---
+++ Ciphers/Ciphers/Utility/TextHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ciphers.Utility
+{
+    public static class TextHelper
+    {
+        public static string NormalizeText(string text)
+        {
+            StringBuilder normalizedText = new StringBuilder(text.Length);
+
+            foreach (var character in text)
+            {
+                if (char.IsLetter(character))
+                {
+                    normalizedText.Append(character);
+                }
+            }
+
+            return normalizedText.ToString();
+        }
+    }
+}
