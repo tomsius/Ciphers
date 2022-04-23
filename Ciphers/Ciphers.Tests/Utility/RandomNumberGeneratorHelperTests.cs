4cedfbb2a6be3576064f2198a76f6640628a693e
Tomas Kašelynas <tomaskas98@gmail.com>
2022-04-23 11:24:18 +0300

    Add tests for RandomNumberGeneratorHelper

---
+++ Ciphers/Ciphers.Tests/Utility/RandomNumberGeneratorHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Ciphers.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciphers.Tests.Utility
+{
+    [TestClass]
+    public class RandomNumberGeneratorHelperTests
+    {
+        [DataRow(26)]
+        [DataRow(3)]
+        [DataRow(1)]
+        [DataTestMethod]
+        public void ConvertTextToVector_ReturnLetterCodeVector_WhenInputIsText(int maximumValue)
+        {
+            RandomNumberGeneratorHelper helper = new();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                int actual = helper.Next(maximumValue);
+
+                Assert.IsTrue(actual >= 0, $"Not true: {actual} >= 0");
+                Assert.IsTrue(actual < maximumValue, $"Not true: {actual} < {maximumValue}");
+            }
+        }
+    }
+}
