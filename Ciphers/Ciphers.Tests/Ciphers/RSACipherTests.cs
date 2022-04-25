495e51047c4da130fb9a47512e5a33c84aea9037
Tomas Kašelynas <tomaskas98@gmail.com>
2022-04-25 13:03:45 +0300

    Add tests for RSACipher

---
+++ Ciphers/Ciphers.Tests/Ciphers/RSACipherTests.cs
@@ -0,0 +1,32 @@
+﻿using Ciphers.Ciphers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ciphers.Tests.Ciphers
+{
+    [TestClass]
+    public class RSACipherTests
+    {
+        [DataRow("A1", "૦୚")]
+        [DataTestMethod]
+        public void Encrypt_ReturnEncryptedMessage_WhenPlainTextIsGiven(string plainText, string expectedEncryption)
+        {
+            RSACipher sender = new(17, 413, 3233);
+            RSACipher receiver = new(17, 413, 3233);
+
+            string actual = sender.Encrypt(plainText, receiver.PublicKey);
+
+            Assert.AreEqual(expectedEncryption, actual);
+        }
+
+        [DataRow("૦୚", "A1")]
+        [DataTestMethod]
+        public void Decrypt_ReturnDecryptedMessage_WhenCipherTextIsGiven(string cipherText, string expectedDecryption)
+        {
+            RSACipher receiver = new(17, 413, 3233);
+
+            string actual = receiver.Decrypt(cipherText);
+
+            Assert.AreEqual(expectedDecryption, actual);
+        }
+    }
+}
