0daf3dbfef85f4cb78b2547dae4482875c6ba2e2
Tomas Kašelynas <tomaskas98@gmail.com>
2022-04-26 11:37:50 +0300

    Add SHA256 hash value calculation

---
+++ Ciphers/Ciphers/Utility/HashCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Ciphers.Utility
+{
+    public static class HashCalculator
+    {
+        public static string CalculateHash(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(input));
+
+                foreach (Byte b in result)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
