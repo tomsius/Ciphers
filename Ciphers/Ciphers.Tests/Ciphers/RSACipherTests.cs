bca6dff13a34073a75031b178190ae5ce04dc7a0
Tomas Kašelynas <tomaskas98@gmail.com>
2022-04-26 11:38:06 +0300

    Add tests for RSA signing

--- Ciphers/Ciphers.Tests/Ciphers/RSACipherTests.cs
+++ Ciphers/Ciphers.Tests/Ciphers/RSACipherTests.cs
@@ -28,5 +28,30 @@ namespace Ciphers.Tests.Ciphers
 
             Assert.AreEqual(expectedDecryption, actual);
         }
+
+        [DataRow("C", "సਨӶ੆Y։կం֯੆ంկġਨġġ֯࠰ਨసச੆֯։ਨ։ޥసġ॥੆ంంկ࠺ਨġġӶ॥॥կ࠰ġޥ࠺։࠰ġկ੆࠰࠰Yసంంਨச॥࠰ص։կ")]
+        [DataRow("AbC123%", "կկկ࠰ంசޥంկ࠺֯֯ంంਨ։ص։సਨկ࠺ంం։࠰Y࠰࠺։ġ੆࠺ص࠰੆࠺։ޥ੆։॥։ޥ࠰Ӷ࠺֯సਨصਨġص࠰ġ॥ص࠺ص࠺॥ਨం")]
+        [DataTestMethod]
+        public void Sign_ReturnSignedHashValue_WhenInputIsGiven(string input, string expectedSignedHashValue)
+        {
+            RSACipher sender = new(17, 413, 3233);
+
+            string actual = sender.Sign(input);
+
+            Assert.AreEqual(expectedSignedHashValue, actual);
+        }
+
+        [DataRow("C", "సਨӶ੆Y։կం֯੆ంկġਨġġ֯࠰ਨసச੆֯։ਨ։ޥసġ॥੆ంంկ࠺ਨġġӶ॥॥կ࠰ġޥ࠺։࠰ġկ੆࠰࠰Yసంంਨச॥࠰ص։կ", true)]
+        [DataRow("AbC123%", "fake", false)]
+        [DataTestMethod]
+        public void IsSignatureValid_ReturnSignatureVerificationResult_WhenInputIsGiven(string input, string signature, bool expectedVerificationResult)
+        {
+            RSACipher sender = new(17, 413, 3233);
+            RSACipher receiver = new(17, 413, 3233);
+
+            bool actual = receiver.IsSignatureValid(input, signature, sender.PublicKey);
+
+            Assert.AreEqual(expectedVerificationResult, actual);
+        }
     }
 }
